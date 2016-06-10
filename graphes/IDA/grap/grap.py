7b7cbfe58a41d74ed1dc21f358d9a53fa7051fb8
Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>
2016-06-10 10:59:12 +0000

    IDA: grap: grap - Changed the stack size.
    Signed-off-by: Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>

--- graphes/IDA/grap/grap.py
+++ graphes/IDA/grap/grap.py
@@ -2,6 +2,7 @@
 """This file is a part of the IDAgrap project."""
 
 import idaapi
+import threading
 
 
 def PLUGIN_ENTRY():
@@ -27,6 +28,17 @@ class IDAgrapPlugin(idaapi.plugin_t):
 
     def run(self, arg):
         """Core of the IDAgrap plugin.
+
+        Args:
+            arg: Plugin argument.
+        """
+        thread = threading.Thread(target=self.main, args=(arg,))
+        # Fixed the stack size to 10M
+        threading.stack_size(0x100000*10)
+        thread.start()
+
+    def main(self, arg):
+        """Main of the IDAgrap plugin.
 
         Args:
             arg: Plugin argument.
