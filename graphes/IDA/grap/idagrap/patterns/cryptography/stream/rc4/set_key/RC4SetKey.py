73be0914af59b9cd3cbe4fd906912f3971527192
Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>
2016-06-30 15:14:49 +0000

    patterns: crypto: rc4 - Added more information about the module.
    Signed-off-by: Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>

--- graphes/IDA/grap/idagrap/patterns/cryptography/stream/rc4/set_key/RC4SetKey.py
+++ graphes/IDA/grap/idagrap/patterns/cryptography/stream/rc4/set_key/RC4SetKey.py
@@ -4,12 +4,29 @@ from os.path import abspath, dirname
 
 from idagrap.modules.Pattern import Pattern, Patterns
 
+# Definition----------------------------------------------------------------
 ROOT = dirname(abspath(__file__))
+
+#
+# Pattern
+#
+# RC4 set key first loop
+loop1 = Pattern(f=ROOT + "/loop1.dot",
+                name="First Loop",
+                description="First Initialization loop of RC4 set_key.")
+
+# RC4 set key second loop
+loop2 = Pattern(f=ROOT + "/loop2.dot",
+                name="Second Loop",
+                description="Second Initialization loop of RC4 set_key.")
+
 
 RC4_SET_KEY = Patterns(
     patterns=[
-        Pattern(ROOT + "/loop1.dot"),  # RC4 set key first loop
-        Pattern(ROOT + "/loop2.dot"),  # RC4 set key second loop
+        loop1,
+        loop2
     ],
-    threshold=1.0
+    threshold=1.0,
+    name="RC4 Set_Key()",
+    description="Initialization function of the RC4 algorithm."
 )
