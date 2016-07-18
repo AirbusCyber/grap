e3b2f33ed2967b7f0e03f783b171a0ad7554d2f4
Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>
2016-07-18 13:37:33 +0000

    ui: IDAgrapForm - Added the plug-in UI.
    Signed-off-by: Jonathan Thieuleux <jonathan.thieuleux.external@cassidian.com>

--- graphes/IDA/grap/grap.py
+++ graphes/IDA/grap/grap.py
@@ -10,6 +10,7 @@ import idaapi
 from idagrap.analysis.Analysis import PatternsAnalysis
 from idagrap.graph.Graph import CFG
 from idagrap.patterns.Modules import MODULES
+from idagrap.ui.IDAgrapForm import IDAgrapForm
 from idc import Wait
 
 sys.setrecursionlimit(400000000)
@@ -45,60 +46,10 @@ class IDAgrapPlugin(idaapi.plugin_t):
         # Wait for the end of autoanalysis
         Wait()
 
-        thread = threading.Thread(target=self.main, args=(arg,))
-        # Fixed the stack size to 10M
-        threading.stack_size(0x100000 * 10)
-        thread.start()
+        # Create form
+        form = IDAgrapForm()
 
-    def main(self, arg):
-        """Main of the IDAgrap plugin.
+        # Show the form
+        form.Show()
 
-        Args:
-            arg: Plugin argument.
-        """
-        cfg = CFG()
-
-        print "[I] Creation of the Control Flow Graph (can take few seconds)"
-        # Get the CFG of the binary
-        cfg.extract()
-
-        print "[I] Searching for patterns."
-        # Group
-        for grp_name, grp in MODULES.iteritems():
-            print "Group: " + grp_name
-
-            # Group->Type
-            for tp_name, tp in grp.iteritems():
-                print "\tType: " + tp_name
-
-                for algo in tp:
-
-                    # print algo
-                    print "\t\tAlgorithm: %s" % algo.get_name()
-
-                    # List of Patterns
-                    for patterns in algo.get_patterns():
-
-                        print "\t\t\tFunction: %s" % patterns.get_name()
-
-                        # List of Pattern
-                        for pattern in patterns.get_patterns():
-                            print "\t\t\t\t[I] Searching for " + pattern.get_name()
-
-                            pattern.parcourir(cfg.graph)
-                            print "\t\t\t\t[I] %d %s pattern found" % (
-                                len(pattern.get_matches()),
-                                pattern.get_name()
-                            )
-
-                        #
-                        # Analysing
-                        #
-                        print "\t\t\t\t[I] Linking the patterns matches which are in the same area"
-                        ana = PatternsAnalysis(patterns)
-
-                        print "\t\t\t\t[I] Filtering those patterns"
-                        ana.filter_patterns()
-                        ana.print_patterns()
-
-        graph_free(cfg.graph, True)
+        return
---
+++ graphes/IDA/grap/idagrap/ui/IDAgrapForm.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# Inspired by IDAscope
+
+import idc
+from idaapi import PluginForm
+from idagrap.config.General import config
+
+from .helpers.ClassCollection import ClassCollection
+from .widgets.CryptoIdentificationWidget import CryptoIdentificationWidget
+
+
+class IDAgrapForm(PluginForm):
+    """IDAgrapForm class.
+
+    This class is the core of the IDAgrap UI.
+
+    Attributes:
+        idagrap_widgets (QMainWindow list): List of widgets to add.
+        cc (ClassCollection): Collection of many classes.
+        icon (QIcon): Icon of the plugin-in.
+        parent (QWidget): QWidget to be used by PySide/PyQt5.
+        tabs (QTabWidget): Stack of tabbed widgets.
+    """
+
+    def __init__(self):
+        """Initialization."""
+        super(IDAgrapForm, self).__init__()
+
+        self.idagrap_widgets = []
+        self.cc = ClassCollection()
+        self.icon = self.cc.QIcon(config['icons_path'] + "circle.png")
+
+    def print_banner(self):
+        """Print the banner."""
+        banner = "{:#^72}\n".format('')
+        banner += " ________  ______   ________   _______    ______    ________   ______    \n"
+        banner += "/_______/\\/_____/\\ /_______/\\ /______/\\  /_____/\\  /_______/\\ /_____/\\   \n"
+        banner += "\\__.::._\\/\\:::_ \\ \\\\::: _  \\ \\\\::::__\\/__\\:::_ \\ \\ \\::: _  \\ \\\\:::_ \\ \\  \n"
+        banner += "   \\::\\ \\  \\:\\ \\ \\ \\\\::(_)  \\ \\\\:\\ /____/\\\\:(_) ) )_\\::(_)  \\ \\\\:(_) \\ \\ \n"
+        banner += "   _\\::\\ \\__\\:\\ \\ \\ \\\\:: __  \\ \\\\:\\\\_  _\\/ \\: __ `\\ \\\\:: __  \\ \\\\: ___\\/ \n"
+        banner += "  /__\\::\\__/\\\\:\\/.:| |\\:.\\ \\  \\ \\\\:\\_\\ \\ \\  \\ \\ `\\ \\ \\\\:.\\ \\  \\ \\\\ \\ \\   \n"
+        banner += "  \\________\\/ \\____/_/ \\__\\/\\__\\/ \\_____\\/   \\_\\/ \\_\\/ \\__\\/\\__\\/ \\_\\/   \n\n"
+        banner += "{:~^72}\n".format('')
+        banner += "{:^72}\n".format('Airbus Defence & Space')
+        banner += "{:#^72}\n".format('')
+
+        print banner
+
+    def setupWidgets(self):
+        """
+        Setup IDAgrap widgets.
+        """
+
+        print "[/] setting up widgets..."
+
+        # Initialization of the widgets
+        self.idagrap_widgets.append(CryptoIdentificationWidget(self))
+        self.setupIDAgrapForm()
+
+        print "[\\] end widgets"
+
+    def setupIDAgrapForm(self):
+        """
+        Orchestrate the already initialized widgets in tabs on the main
+        window.
+        """
+        self.tabs = self.cc.QTabWidget()
+        self.tabs.setTabsClosable(False)
+
+        # Add all the widgets
+        for widget in self.idagrap_widgets:
+            self.tabs.addTab(widget, widget.icon, widget.name)
+
+        # Lines up widgets
+        layout = self.cc.QVBoxLayout()
+        layout.addWidget(self.tabs)
+
+        # Set layout in QWidget
+        self.parent.setLayout(layout)
+
+    def OnCreate(self, form):
+        """OnCreate of IDAgrap.
+
+        This event is called when the plugin form is created.
+
+        Arguments:
+            form (TForm*): Plug-in form.
+        """
+        self.print_banner()
+
+        # compatibility with IDA < 6.9
+        # Convert TForm* for PyQt or PySide
+        try:
+            self.parent = self.FormToPySideWidget(form)
+        except Exception:
+            self.parent = self.FormToPyQtWidget(form)
+
+        self.parent.setWindowIcon(self.icon)
+        self.setupWidgets()
+
+    def Show(self):
+        """
+        Creates the form and brings it to the front.
+        """
+        if idc.GetInputMD5() is None:
+            return
+        else:
+            name = "{} {}".format(config['name'], config['version'])
+            options = PluginForm.FORM_CLOSE_LATER |\
+                      PluginForm.FORM_SAVE |\
+                      PluginForm.FORM_RESTORE
+
+            return PluginForm.Show(self, name, options=options)
