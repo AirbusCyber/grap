0f825e6b570fdc9f93102f504cca923997ea5577
Aurelien Thierry <aurelien@athierry.fr>
2016-10-18 08:39:49 +0200

    Handle multiple test graphs input.

--- graphes/tools/grap-match/grap-match.py
+++ graphes/tools/grap-match/grap-match.py
@@ -17,7 +17,7 @@ def parse_arguments():
     parser.add_argument("-ncl", "--nochecklabels", action="store_false",
                         help="do not check the labels of sites")
     parser.add_argument("patternFile", help="The pattern to search")
-    parser.add_argument("testFile", help="The file in which we will lookfor the pattern")
+    parser.add_argument("testFile", nargs='+', help="The file in which we will lookfor the pattern")
 
     return parser.parse_args()
 
@@ -41,57 +41,58 @@ def main():
                                n_pattern)
     if args.verbosity >= 2:
         print("Pattern Parcours is:\n" + parcours.toString() + "\n")
-
-    if args.verbosity >= 1:
-        print("Parsing test file.")
-
-    test_graph = getGraphFromFile(args.testFile)
-    n_test = test_graph.nodes.size
-
-    if args.verbosity >= 1:
-        print("Done.")
-
-    rt = parcours.parcourir(test_graph, n_pattern, args.nochecklabels,
-                            True, not args.quiet, False)
-    count = rt.first
-
+    
     if not args.quiet:
-        print("%d traversal(s) possible in %s." % (count, args.testFile))
-        print("Pattern graph (%s) has %d nodes.\nTest graph (%s) has %d nodes." %
-              (args.patternFile, n_pattern, args.testFile, n_test))
-    else:
-        print("%s %d" % (args.testFile, count))
+        print("Pattern graph (%s) has %d nodes." % (args.patternFile, n_pattern))
 
-    set_gotten = rt.second
+    for test_path in args.testFile:
+        if args.verbosity >= 1:
+            print("Parsing test file.")
 
-    if not set_gotten.empty():
-        print("\nExtracted nodes:")
+        test_graph = getGraphFromFile(test_path)
+        n_test = test_graph.nodes.size
 
-        # For each match
-        for f_index, found_nodes in enumerate(set_gotten, start=1):
-            print("Match %d" % f_index)
+        if args.verbosity >= 1:
+            print("Done.")
 
-            for getid, node_list in found_nodes.iteritems():
-                if not node_list.empty():
-                    for n_index, node in enumerate(node_list):
-                        s = str(getid)
+        rt = parcours.parcourir(test_graph, n_pattern, args.nochecklabels, True, not args.quiet, False)
+        count = rt.first
 
-                        if node_list.size() > 1:
-                            s += str(n_index)
+        if not args.quiet:
+            print("\nTest graph (%s) has %d nodes." % (test_path, n_test))
+            print("%d traversal(s) possible in %s." % (count, test_path))
+        elif count >= 1:
+            print("%s %d" % (test_path, count))
 
-                        s += ": "
+        set_gotten = rt.second
 
-                        if node.info.has_address:
-                            s += hex(node.info.address) + ", "
+        if not set_gotten.empty():
+            print("\nExtracted nodes:")
 
-                        s += node.info.inst_str
-                        print s
+            # For each match
+            for f_index, found_nodes in enumerate(set_gotten, start=1):
+                print("Match %d" % f_index)
 
-        freeMapGotten(found_nodes)
+                for getid, node_list in found_nodes.iteritems():
+                    if not node_list.empty():
+                        for n_index, node in enumerate(node_list):
+                            s = str(getid)
 
-    parcours.freeParcours(True)
+                            if node_list.size() > 1:
+                                s += str(n_index)
+
+                            s += ": "
+
+                            if node.info.has_address:
+                                s += hex(node.info.address) + ", "
+
+                            s += node.info.inst_str
+                            print s
+
+                freeMapGotten(found_nodes)
+        del(set_gotten)
+        graph_free(test_graph, True)
     graph_free(pattern_graph, True)
-    graph_free(test_graph, True)
 
 if __name__ == '__main__':
     main()
