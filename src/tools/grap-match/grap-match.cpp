be13c52aceee5b917cbd3ad0b5fe0e8ee6bea43e
Aurelien Thierry <aurelien@athierry.fr>
2016-12-23 21:24:38 +0100

    Added "seccomp" system call to seccomp filters

--- src/tools/grap-match/grap-match.cpp
+++ src/tools/grap-match/grap-match.cpp
@@ -47,8 +47,9 @@ void drop_initial_privileges(){
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(futex), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(set_robust_list), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(madvise), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(prctl), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mremap), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(prctl), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(seccomp), 0);
   
   int r = seccomp_load(ctx);
   RELEASE_ASSERT(r == 0);
@@ -63,6 +64,7 @@ void drop_privileges(){
 
   seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 0);
   seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(prctl), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(seccomp), 0);
   
   int r = seccomp_load(ctx);
   RELEASE_ASSERT(r == 0);
